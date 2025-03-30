979e26eea0c96433b6996dba17734a25a9b195b3
Tomsh666 <tomshinskiy02@gmail.com>
2025-03-31 00:11:15 +0300

    work: button for uploading values and measure

---
+++ mesure.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func StartMeasuring() {
+	// TODO: Добавить логику для старта измерения
+	fmt.Println("measuring")
+}
---
+++ table.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func UploadValuesToTable() {
+	// TODO: Добавить логику для вывода значений в таблицу
+	fmt.Println("Uploading")
+}
